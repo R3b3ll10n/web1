006daec2ee6244b934f9a5a79361571788a3ee9f
elikito <elias.kitano@gmail.com>
2020-10-09 13:35:27 +0200

    incluye script registros_usuarios

---
+++ utilidades/registros_usuarios.sql
@@ -0,0 +1,9 @@
+/* tabla de registros usuarios*/
+
+CREATE TABLE `registros_usuarios` (
+	`id` INT(4),
+	`name` VARCHAR(30),
+	`lastname` VARCHAR(30),
+	`password` VARCHAR(15),
+	PRIMARY KEY (`id`)
+);
